4d5c44fc417b587a8062dbc5ec23bfe13065618e
AKSHAYSHIKARE <91713968+akshu0809@users.noreply.github.com>
2024-02-29 12:43:57 +0530

    PC
    Commit

---
+++ StudentManagement.sql
@@ -0,0 +1,7 @@
+Create database miniproject;
+
+use miniproject;
+
+create table student(sid int primary key auto_increment,sname varchar(250),sphone varchar(250), scity varchar(250));
+
+select*from student;
